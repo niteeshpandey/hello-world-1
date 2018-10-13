86bccc5d02a13a4b685e61274487d49ab0b01bf0
niteeshpandey <33634467+niteeshpandey@users.noreply.github.com>
2018-10-13 10:01:56 +0530

    print company order

---
+++ enum function.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+enum company(GOOGLE,APPLE,GUN,BOOOT,GOO)
+enum google =GOOGLE;
+enum ju =JU;
+printf("enum company gun is %d",gun);
+getch();
+}
